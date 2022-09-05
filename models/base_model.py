c2d2d293508551372c618a08b74a8d53f3a58bd8
LikemDzokoto <likemdzokoto@gmail.com>
2022-09-04 22:43:32 -0700

    refactor base_model

--- models/base_model.py
+++ models/base_model.py
@@ -17,15 +17,16 @@ class BaseModel():
     """
     def __init__(self, *args, **kwargs):
         ''' inits this instance '''
-        for k, v in kwargs.items():
-            if k in ('created_at', 'updated_at'):
-                v = datetime.fromisoformat(v)
-            if k != '__class__':
-                setattr(self, k, v)
-            else:
-                pass
+        t_format = '%Y-%m-%dT%H:%M:%S.%f'
 
-        if self.id is None:
+        if kwargs:
+            for k,v in kwargs.items():
+                if k == "created_at" or k=="updated_at":
+                    v = datetime.strptime(v,t_format)
+                elif k =="__class__":
+                    continue
+                setattr(self, k,v)
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
