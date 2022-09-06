1aa373c5f9e02f3dbb5f15d0c2ddc6a97bf58488
LikemDzokoto <likemdzokoto@gmail.com>
2022-09-06 15:21:56 -0700

    amenity class

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines the amenity class
+"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    A subclass of BaseModel class
+
+    Attributes:
+    name(str):The name of the amenity:
+    """
+
+    name = ""
